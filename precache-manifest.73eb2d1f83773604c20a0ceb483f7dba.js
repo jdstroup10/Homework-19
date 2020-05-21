6267e90620d6bb439eba98731b9c3f56097e74ca
Jason Strouphauer <jdstroup10@gmail.com>
2020-05-21 13:21:40 -0500

    Updates

---
+++ precache-manifest.73eb2d1f83773604c20a0ceb483f7dba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27ce2a9d46aa598ed45ed6209fa28be",
+    "url": "/Homework-19/index.html"
+  },
+  {
+    "revision": "dc0bf83dddfcd3186649",
+    "url": "/Homework-19/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "566933b6bbf35a01ecf6",
+    "url": "/Homework-19/static/js/2.8e15f791.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Homework-19/static/js/2.8e15f791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc0bf83dddfcd3186649",
+    "url": "/Homework-19/static/js/main.b7685be9.chunk.js"
+  },
+  {
+    "revision": "5d764112a6e61c41e36a",
+    "url": "/Homework-19/static/js/runtime-main.4cc51a16.js"
+  }
+]);
